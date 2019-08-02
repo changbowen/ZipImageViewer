5eb9e8c39e594f4f68266f1d3b1432e7a3ab6290
Carl Chang <changbowen@msn.cn>
2019-08-02 21:25:24 +0800

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZipImageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => AddImages(new [] { @"\\192.168.1.250\hdd_private\Pictures\苏夏妞妞" }));
+
+        }
+
+        private void MainWin_Drop(object sender, DragEventArgs e)
+        {
+            var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            Task.Run(() => AddImages(paths));
+        }
+
+        private void AddImages(string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                // check if path is a file or directory
+                if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+                {
+                    //directory
+                    foreach (var file in Directory.GetFiles(path, "*.jpg", SearchOption.TopDirectoryOnly))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            var tn = new Thumbnail(file);
+                            WP1.Children.Add(tn);
+                        }, System.Windows.Threading.DispatcherPriority.Background);
+                        
+                    }
+                }
+                else
+                {
+                    //file
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Thumbnail.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZipImageViewer
+{
+    public partial class Thumbnail : UserControl
+    {
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+        public static readonly DependencyProperty FilePathProperty =
+            DependencyProperty.Register("FilePath", typeof(string), typeof(Thumbnail), new PropertyMetadata(null));
+
+
+        public Thumbnail(string filepath)
+        {
+            FilePath = filepath;
+            InitializeComponent();
+        }
+    }
+}
