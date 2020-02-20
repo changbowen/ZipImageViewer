df24f86a997fa83e3d7a55e6ecf62b689f2207f2
Carl Chang <changbowen@msn.cn>
2020-02-21 00:40:13 +0800

    minor change to borderless window style; add information to context menu; update fontawesome;

--- ContextMenuWindow.xaml.cs
+++ ContextMenuWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,13 +15,25 @@ using System.Windows.Shapes;
 
 namespace ZipImageViewer
 {
-    public partial class ContextMenuWindow : RoundedWindow
+    public partial class ContextMenuWindow : RoundedWindow, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
         public ContextMenuWindow() {
             InitializeComponent();
         }
 
-        public ObjectInfo ObjectInfo;
+
+        private ObjectInfo objectInfo;
+        public ObjectInfo ObjectInfo {
+            get => objectInfo;
+            set {
+                if (objectInfo == value) return;
+                objectInfo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ObjectInfo)));
+            }
+        }
+
 
         private void Menu_PreviewMouseUp(object sender, MouseButtonEventArgs e) {
             if (ObjectInfo == null) return;
